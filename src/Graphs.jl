2c8e04ef23d392c44e48d0e11aa2f25c69857202
Umberto Zerbinati <uzerbinati@me.com>
2020-02-09 19:12:28 +0100

    Correction on Numerical Test pt.3

--- src/Graphs.jl
+++ src/Graphs.jl
@@ -1,82 +1,83 @@
-opt = readline("Which set of test?");
-if opt==1
-	try
-		print("TEST 1\n");
-		NumJL.WaveBanch(1)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 2\n");
-		NumJL.WaveBanch(2)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 3\n");
-		NumJL.WaveBanch(3)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 4\n");
-		NumJL.WaveBanch(4)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 5\n");
-		NumJL.WaveBanch(5)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 6\n");
-		NumJL.WaveBanch(6)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 7\n");
-		NumJL.WaveBanch(7)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 9\n");
-		NumJL.WaveBanch(9)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 10\n");
-		NumJL.WaveBanch(10)
-	catch e
-		print(e)
-	end
-elseif opt == 2
-	try
-		print("TEST 9\n");
-		NumJL.WaveBanch(9)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 11\n");
-		NumJL.WaveBanch(11)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 12\n");
-		NumJL.WaveBanch(12)
-	catch e
-		print(e)
-	end
-	try
-		print("TEST 13\n");
-		NumJL.WaveBanch(13)
-	catch e
-		print(e)
+function Graph(opt)
+	if opt==1
+		try
+			print("TEST 1\n");
+			NumJL.WaveBanch(1)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 2\n");
+			NumJL.WaveBanch(2)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 3\n");
+			NumJL.WaveBanch(3)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 4\n");
+			NumJL.WaveBanch(4)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 5\n");
+			NumJL.WaveBanch(5)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 6\n");
+			NumJL.WaveBanch(6)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 7\n");
+			NumJL.WaveBanch(7)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 9\n");
+			NumJL.WaveBanch(9)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 10\n");
+			NumJL.WaveBanch(10)
+		catch e
+			print(e)
+		end
+	elseif opt == 2
+		try
+			print("TEST 9\n");
+			NumJL.WaveBanch(9)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 11\n");
+			NumJL.WaveBanch(11)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 12\n");
+			NumJL.WaveBanch(12)
+		catch e
+			print(e)
+		end
+		try
+			print("TEST 13\n");
+			NumJL.WaveBanch(13)
+		catch e
+			print(e)
+		end
 	end
 end
